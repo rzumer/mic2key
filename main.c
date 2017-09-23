df0b6fe8fbfe851a8e110978f262cb18f36b5606
Raphael Zumer <rzumer@gmail.com>
2017-09-23 12:53:50 -0400

    Add thresholded keyboard input

--- main.c
+++ main.c
@@ -2,6 +2,34 @@
 #include <Mmdeviceapi.h>
 #include <audioclient.h>
 
+// Process bytes and return the peak amplitude normalized on a scale of 100.
+int GetAmplitude(WAVEFORMATEX *format, BYTE *data, UINT32 *numFrames)
+{
+	/*printf("%u\n", *numFrames);
+	printf("%u\n", sizeof(*data));
+	printf("%u\n", format->wBitsPerSample);
+	printf("%u\n", format->nChannels);
+	printf("%u\n", format->cbSize);*/
+	/*
+	switch (format->wFormatTag)
+	{
+	case WAVE_FORMAT_PCM:
+		printf("pcm");
+		break;
+	case WAVE_FORMAT_EXTENSIBLE:
+		printf("extensible");
+		break;
+	case WAVE_FORMAT_MPEG:
+		printf("mpeg");
+		break;
+	case WAVE_FORMAT_MPEGLAYER3:
+		printf("mp3");
+		break;
+	}*/
+
+	return 0;
+}
+
 // Record an audio stream from the default audio capture
 // device. Poll the peak intensity every 1/10 second and
 // toggle the specified key when the intensity exceeds a
@@ -11,6 +39,15 @@
 // REFERENCE_TIME time units per second and per millisecond
 #define REFTIMES_PER_SEC 10000000
 #define REFTIMES_PER_MILLISEC 10000
+
+// Maximum buffer length in milliseconds
+#define MAX_BUFFER_LENGTH 10000
+
+// Minimm and maximum values for the threshold parameter
+#define MIN_THRESHOLD_VALUE 0
+#define MAX_THRESHOLD_VALUE 100
+
+#define MAX(a,b) ((a) > (b) ? a : b)
 
 #define EXIT_ON_ERROR(hres) \
 if (FAILED(hres)) { goto Exit; }
@@ -22,10 +59,21 @@ static const GUID IID_IMMDeviceEnumerator = { 0xA95664D2, 0x9614, 0x4F35, 0xA7, 
 static const GUID IID_IAudioClient = { 0x1CB9AD4C, 0xDBFA, 0x4C32, 0xB1, 0x78, 0xC2, 0xF5, 0x68, 0xA7, 0x03, 0xB2 };
 static const GUID IID_IAudioCaptureClient = { 0xC8ADBD64, 0xE71E, 0x48A0, 0xA4, 0xDE, 0x18, 0x5C, 0x39, 0x5C, 0xD3, 0x17 };
 
-HRESULT RecordAudioStream(/*MyAudioSink *pMySink*/)
+HRESULT RecordAudioStream(int timeUnit, int threshold, WORD keyCode)
 {
+	if (timeUnit <= 0 || timeUnit > MAX_BUFFER_LENGTH)
+	{
+		printf("Recorded time unit is out of permitted range.");
+		return E_INVALIDARG;
+	}
+	if (threshold < MIN_THRESHOLD_VALUE || threshold > MAX_THRESHOLD_VALUE)
+	{
+		printf("Threshold is out of range.");
+		return E_INVALIDARG;
+	}
+
 	HRESULT hr;
-	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_SEC / 5;
+	REFERENCE_TIME hnsRequestedDuration = REFTIMES_PER_MILLISEC * timeUnit;
 	REFERENCE_TIME hnsActualDuration;
 	UINT32 bufferFrameCount;
 	UINT32 numFramesAvailable;
@@ -38,6 +86,13 @@ HRESULT RecordAudioStream(/*MyAudioSink *pMySink*/)
 	BOOL bDone = FALSE;
 	BYTE *pData;
 	DWORD flags;
+	int peak = 0;
+	INPUT input = { .type = INPUT_KEYBOARD,
+		.ki.wScan = MapVirtualKey(keyCode, MAPVK_VK_TO_VSC),
+		.ki.time = 0,
+		.ki.dwExtraInfo = 0,
+		.ki.wVk = keyCode,
+		.ki.dwFlags = 0 };
 
 	CoInitialize(0);
 
@@ -63,10 +118,6 @@ HRESULT RecordAudioStream(/*MyAudioSink *pMySink*/)
 	hr = pAudioClient->lpVtbl->GetService(pAudioClient, &IID_IAudioCaptureClient, (void**)&pCaptureClient);
 	EXIT_ON_ERROR(hr)
 
-	// Notify the audio sink which format to use.
-	/*hr = pMySink->SetFormat(pwfx);
-	EXIT_ON_ERROR(hr)*/
-
 	// Calculate the actual duration of the allocated buffer.
 	hnsActualDuration = (double)REFTIMES_PER_SEC * bufferFrameCount / pwfx->nSamplesPerSec;
 
@@ -82,6 +133,9 @@ HRESULT RecordAudioStream(/*MyAudioSink *pMySink*/)
 		hr = pCaptureClient->lpVtbl->GetNextPacketSize(pCaptureClient, &packetLength);
 		EXIT_ON_ERROR(hr)
 
+		// Reset the peak value.
+		peak = 0;
+
 		while (packetLength != 0)
 		{
 			// Get the available data in the shared buffer.
@@ -90,15 +144,15 @@ HRESULT RecordAudioStream(/*MyAudioSink *pMySink*/)
 
 			if (flags & AUDCLNT_BUFFERFLAGS_SILENT)
 			{
-				pData = NULL;  // Tell CopyData to write silence.
+				pData = 0;
 			}
 
-			// Copy the available capture data to the audio sink.
-			if (pData == NULL) printf("Null data");
-			else printf("%02x\n", pData);
+			// Concatenate bytes
+			//if (pData == NULL) printf("Null data");
+			//else printf("%02x\n", pData);
 
-			bDone = TRUE; //hr = pMySink->CopyData(pData, numFramesAvailable, &bDone);
-			EXIT_ON_ERROR(hr)
+			int amplitude = GetAmplitude(pwfx, pData, &numFramesAvailable);
+			peak = MAX(peak, amplitude);
 
 			hr = pCaptureClient->lpVtbl->ReleaseBuffer(pCaptureClient, numFramesAvailable);
 			EXIT_ON_ERROR(hr)
@@ -106,6 +160,22 @@ HRESULT RecordAudioStream(/*MyAudioSink *pMySink*/)
 			hr = pCaptureClient->lpVtbl->GetNextPacketSize(pCaptureClient, &packetLength);
 			EXIT_ON_ERROR(hr)
 		}
+
+		if (peak >= threshold)
+		{
+			printf("Y\n");
+			input.ki.dwFlags = 0;
+		}
+		else
+		{
+			printf("N\n");
+			input.ki.dwFlags = KEYEVENTF_KEYUP;
+		}
+
+		// Send input (key down or up depending on peak value).
+		SendInput(1, &input, sizeof(INPUT));
+
+		bDone = TRUE;
 	}
 
 	hr = pAudioClient->lpVtbl->Stop(pAudioClient);  // Stop recording.
@@ -122,7 +192,7 @@ Exit:
 }
 
 int main() {
-	RecordAudioStream();
+	RecordAudioStream(100, 50, VK_SPACE);
 	getchar();
 	return 0;
 }
